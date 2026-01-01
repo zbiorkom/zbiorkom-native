c5dd179dd45d02ac2c584b32a5979de51425fbbe
Dominik Skarżyński <dominik.skarzynski@execon.pl>
2026-01-01 21:37:00 +0100

    feat

--- src/components/ui/IconButton.tsx
+++ src/components/ui/IconButton.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
 import { Icon, TouchableRipple } from "react-native-paper";
 import { useTheme } from "~/hooks/useTheme";
@@ -5,26 +6,27 @@ import { useTheme } from "~/hooks/useTheme";
 type Props = {
     icon: string;
     onPress?: () => void;
+    square?: boolean;
 };
 
-export default ({ icon, onPress }: Props) => {
+export default ({ icon, onPress, square }: Props) => {
+    const [isPressed, setIsPressed] = useState(false);
     const { theme } = useTheme();
 
     return (
         <TouchableRipple
-            style={[styles.iconButton, { backgroundColor: theme.colors.secondaryContainer }]}
+            style={{
+                backgroundColor: theme.colors.secondaryContainer,
+                borderRadius: isPressed ? 12 : 24,
+                paddingHorizontal: square ? 12 : 6,
+                paddingVertical: 12,
+            }}
             borderless
             onPress={onPress}
+            onPressIn={() => setIsPressed(true)}
+            onPressOut={() => setIsPressed(false)}
         >
             <Icon source={icon} size={22} color={theme.colors.onSecondaryContainer} />
         </TouchableRipple>
     );
 };
-
-const styles = StyleSheet.create({
-    iconButton: {
-        paddingHorizontal: 6,
-        paddingVertical: 12,
-        borderRadius: 24,
-    },
-});
