c2f2911479b9091ddbc6b0618ea5f1a69c57e7f3
♿ <domeqrecords+gh@gmail.com>
2026-02-11 21:56:04 +0100

    fix delay colors

--- src/components/ui/DelayText.tsx
+++ src/components/ui/DelayText.tsx
@@ -8,19 +8,23 @@ type Props = {
     delay?: number;
     status?: StopDepartureStatus;
     darkMode?: boolean;
-}
+};
 
 export default ({ delay, status, darkMode }: Props) => {
     const { t } = useTranslation("punctuality");
 
     const [color, text] = useMemo(() => {
-        if (status === StopDepartureStatus.Cancelled) return ["#fff", t("cancelled")];
-        if (status === StopDepartureStatus.Scheduled || !delay) return ["#fff", t("unknown")];
+        if (status === StopDepartureStatus.Cancelled) {
+            return [darkMode ? "#e1bee7" : "#4a148c", t("cancelled")];
+        } else if (status === StopDepartureStatus.Scheduled || !delay) {
+            return [darkMode ? "#cfd8dc" : "#455a64", t("unknown")];
+        } else if (delay >= 60000) {
+            return [darkMode ? "#ff8a80" : "#b71c1c", t("late", { time: msToTime(delay) })];
+        } else if (delay <= -60000) {
+            return [darkMode ? "#ffe082" : "#d84315", t("early", { time: msToTime(-delay) })];
+        }
 
-        if (delay >= 60000) return [darkMode ? "#ff6966" : "#ff6966", t("late", { time: msToTime(delay) })];
-        if (delay <= -60000) return [darkMode ? "#ff9500" : "#ff9500", t("early", { time: msToTime(-delay) })];
-
-        return [darkMode ? "#65e765" : "#65e765", t("onTime")];
+        return [darkMode ? "#c5e1a5" : "#1b5e20", t("onTime")];
     }, [delay, status]);
 
     return (
